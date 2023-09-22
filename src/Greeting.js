14fa69241c178702692ef9e7bc4574e7bf68a8d2
Arzuag-a <rarzuaga@unal.edu.co>
2023-09-22 17:15:29 -0500

    Inicio

---
+++ src/Greeting.js
@@ -0,0 +1,19 @@
+export function Greeting({title, name = "User"}) {
+    console.log(title)
+    return <h1>{title}</h1>
+}
+
+export function UserCard({name, amount, married, address, greet}) {
+    console.log(name, amount, married, address, greet)
+    return (
+    <div>
+        <h1>{name}</h1>
+        <p>💵{amount}</p>
+        <p>{married ? "married": "single"}</p>
+        <ul>
+            <li>City: {address.city}</li>
+            <li>Address: {address.street}</li>
+        </ul>
+    </div>
+    )
+}
---
+++ src/Task.js
@@ -0,0 +1,13 @@
+import './Task.css'
+
+
+export function TaskCard(ready){
+    return (
+    <div className='card'>
+        <h1>Mi primera tarea</h1>
+        <span style={ready ? {background: "green"} : {background: "red"}}>
+            {ready ? 'Tarea realizada' : 'Tarea pendiente'}
+        </span>
+    </div>
+    )
+}
